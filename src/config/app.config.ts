ca63345926a758c728bfca2dd9355da792f73bee
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-04-19 10:18:37 -0400

    fixed code smells

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,12 +1,12 @@
 import { registerAs } from '@nestjs/config';
 
-const path = process.env.EASEY_MDM_API_PATH || 'api/master-data-mgmt'
+const path = process.env.EASEY_MDM_API_PATH || 'api/master-data-mgmt';
 const host = process.env.EASEY_MDM_API_HOST || 'localhost';
 const port = process.env.EASEY_MDM_API_PORT || 8080;
 
-let uri = `https://${host}/${path}`
+let uri = `https://${host}/${path}`;
 
-if (host == 'localhost') {
+if (host === 'localhost') {
   uri = `http://localhost:${port}/${path}`;
 }
 
