5989d01ef168e32ed869e60d1b9f93a828ef73e3
Jeffrey Aggrey <jeffreyaggrey@gmailcom>
2021-06-27 08:08:46 -0500

    .

--- src/bypass-approach-codes/bypass-approach-codes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BypassApproachCodesController } from './bypass-approach-codes.controller';
-import { BypassApproachCodesService } from './bypass-approach-codes.service';
-
-@Module({
-  controllers: [BypassApproachCodesController],
-  providers: [BypassApproachCodesService]
-})
-export class BypassApproachCodesModule {}
