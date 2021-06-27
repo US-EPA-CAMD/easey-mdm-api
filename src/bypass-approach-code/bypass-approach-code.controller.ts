2da204eef1d8e5886dfd1634e87b5db318bd0653
Jeffrey Aggrey <jeffreyaggrey@gmailcom>
2021-06-27 08:56:53 -0500

    add updateBypassApproachCode

--- src/bypass-approach-code/bypass-approach-code.controller.ts
+++ src/bypass-approach-code/bypass-approach-code.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
 import { ApiBadRequestResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { BypassApproachCodeDTO } from 'src/dto/bypass-approach-code.dto';
 import { BypassApproachCodeService } from './bypass-approach-code.service';
@@ -6,9 +6,7 @@ import { BypassApproachCodeService } from './bypass-approach-code.service';
 @ApiTags('Bypass Approach Codes')
 @Controller()
 export class BypassApproachCodesController {
-  constructor(
-    private readonly bypassApproachCodeService: BypassApproachCodeService,
-  ) {}
+  constructor(private readonly service: BypassApproachCodeService) {}
 
   @Get()
   @ApiOkResponse({
@@ -18,6 +16,20 @@ export class BypassApproachCodesController {
     description: 'Invalid Request',
   })
   getAllBypassApproachCodes(): Promise<BypassApproachCodeDTO[]> {
-    return this.bypassApproachCodeService.getAllBypassApproachCodes();
+    return this.service.getAllBypassApproachCodes();
+  }
+
+  @Put('/:id')
+  @ApiOkResponse({
+    description: 'Updates Bypass-Approach Code',
+  })
+  @ApiBadRequestResponse({
+    description: 'Invalid Request',
+  })
+  updateBypassApproachCode(
+    @Param('id') id: string,
+    @Body() payload: BypassApproachCodeDTO,
+  ): Promise<BypassApproachCodeDTO> {
+    return this.service.updateBypassApproachCode(id, payload);
   }
 }
--- src/bypass-approach-code/bypass-approach-code.service.ts
+++ src/bypass-approach-code/bypass-approach-code.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BypassApproachCodeDTO } from 'src/dto/bypass-approach-code.dto';
 import { BypassApproachCodeRepository } from './bypass-approach-code.repository';
@@ -7,12 +7,34 @@ import { BypassApproachCodeRepository } from './bypass-approach-code.repository'
 export class BypassApproachCodeService {
   constructor(
     @InjectRepository(BypassApproachCodeRepository)
-    private readonly repository: BypassApproachCodeRepository
+    private readonly repository: BypassApproachCodeRepository,
   ) {}
 
   async getAllBypassApproachCodes(): Promise<BypassApproachCodeDTO[]> {
-    const result = await this.repository.getAllBypassApproachCodes()
+    const result = await this.repository.getAllBypassApproachCodes();
 
     return result;
+  }
+
+  async getBypassApproachCode(id: string): Promise<BypassApproachCodeDTO> {
+    const result = this.repository.findOne(id)
+
+    if (!result) {
+      throw new NotFoundException('Invalid Request')
+    }
+
+    return result
+  }
+
+  async updateBypassApproachCode(
+    id: string,
+    payload: BypassApproachCodeDTO,
+  ): Promise<BypassApproachCodeDTO> {
+    const result = await this.getBypassApproachCode(id)
+    
+    result.bypassApproachCode = id,
+    result.bypassApproachCodeDescription = payload.bypassApproachCode
+
+    return await this.repository.save(result)
   }
 }
