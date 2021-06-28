d85f09f81f0393c4abd38c1d0b9ac6d0bf303a40
Jason Whitehead <66504934+jwhitehead77@users.noreply.github.com>
2021-06-28 16:17:16 -0500

    Update sub-data-code.controller.ts
    removed path parameter value in @Controller() decorator causing a double path & removed POST, PUT, DELETE methods

--- src/sub-data-code/sub-data-code.controller.ts
+++ src/sub-data-code/sub-data-code.controller.ts
@@ -11,14 +11,9 @@ import { SubDataCodeService } from './sub-data-code.service';
 import { SubDataCodeDto } from '../dto/sub-data-code.dto';
 import { ApiBadRequestResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 @ApiTags('Substitute Data Codes')
-@Controller('sub-data-code')
+@Controller()
 export class SubDataCodeController {
   constructor(private readonly subDataCodeService: SubDataCodeService) {}
-
-  // @Post()
-  // create(@Body() createSubDataCodeDto: SubDataCodeDto) {
-  //   return this.subDataCodeService.create(createSubDataCodeDto);
-  // }
 
   @Get()
   @ApiOkResponse({
@@ -32,24 +27,4 @@ export class SubDataCodeController {
   getSubDataCodes(): Promise<SubDataCodeDto[]> {
     return this.subDataCodeService.getSubDataCodes();
   }
-
-  @Put(':id')
-  @ApiOkResponse({
-    type: SubDataCodeDto,
-    description: 'Updates Substitue Data Code',
-  })
-  @ApiBadRequestResponse({
-    description: 'Invalid Request',
-  })
-  updateSubDataCode(
-    @Param('id') id: string,
-    @Body() updateSubDataCodeDto: SubDataCodeDto,
-  ): Promise<SubDataCodeDto> {
-    return this.subDataCodeService.updateSubDataCode(id, updateSubDataCodeDto);
-  }
-
-  // @Delete(':id')
-  // removeSubDataCode(@Param('id') id: string) {
-  //   return this.subDataCodeService.removeSubDataCode(id);
-  // }
 }
