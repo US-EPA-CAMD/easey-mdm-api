b1018319e6cfde99dd05fd56b088462aa2ef1e53
Jeffrey Aggrey <jeffreyaggrey@gmailcom>
2021-07-02 17:33:07 -0500

    handle code smells

--- src/bypass-approach-code/bypass-approach-code.service.ts
+++ src/bypass-approach-code/bypass-approach-code.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { BypassApproachCodeDTO } from '../dto/bypass-approach-code.dto';
@@ -12,30 +12,6 @@ export class BypassApproachCodeService {
   ) {}
 
   async getBypassApproachCodes(): Promise<BypassApproachCodeDTO[]> {
-    const result = await this.repository.getBypassApproachCodes();
-
-    return result;
+    return this.repository.getBypassApproachCodes();
   }
-
-  // async getBypassApproachCode(id: string): Promise<BypassApproachCodeDTO> {
-  //   const result = this.repository.findOne(id);
-
-  //   if (!result) {
-  //     throw new NotFoundException('Invalid Request');
-  //   }
-
-  //   return result;
-  // }
-
-  // async updateBypassApproachCode(
-  //   id: string,
-  //   payload: BypassApproachCodeDTO,
-  // ): Promise<BypassApproachCodeDTO> {
-  //   const result = await this.getBypassApproachCode(id);
-
-  //   (result.bypassApproachCode = id),
-  //     (result.bypassApproachCodeDescription = payload.bypassApproachCode);
-
-  //   return await this.repository.save(result);
-  // }
 }
