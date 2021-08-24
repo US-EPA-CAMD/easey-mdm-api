2636baf4c44d5fbf4c6c98415540fcd4a574b6d3
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-08-24 15:54:11 -0400

    fixed import code smell

--- src/maps/transaction-type.map.ts
+++ src/maps/transaction-type.map.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 import { BaseMap } from './base.map';
 import { TransactionType } from '../entities/transaction-type-code.entity';
-import { TransactionTypeDTO } from 'src/dto/transaction-type.dto';
+import { TransactionTypeDTO } from '../dto/transaction-type.dto';
 
 @Injectable()
 export class TransactionTypeMap extends BaseMap<TransactionType, TransactionTypeDTO> {
