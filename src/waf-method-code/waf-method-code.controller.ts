7d6fa251f789b981e55ca9b17939a1eedc689549
Dion Olympia <dionolympia@cvpcorp.com>
2021-10-13 14:08:59 -0400

    added get endpoint for wad method codes

--- src/waf-method-code/waf-method-code.controller.ts
+++ src/waf-method-code/waf-method-code.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { WafMethodCodeService } from './waf-method-code.service';
+import { WafMethodCodeDTO } from '../dto/waf-method-code.dto';
 
-@Controller('waf-method-code')
-export class WafMethodCodeController {}
+@ApiTags('WAF Method Codes')
+@Controller()
+export class WafMethodCodeController {
+  constructor(private readonly service: WafMethodCodeService) {}
+
+  @Get()
+  @ApiOkResponse({
+    isArray: true,
+    type: WafMethodCodeDTO,
+    description: 'Retrieves all unit of measure codes.',
+  })
+  @ApiBadRequestResponse({
+    description: 'Invalid Request',
+  })
+  getUnitofMeasureCodes(): Promise<WafMethodCodeDTO[]> {
+    return this.service.getWafMethodCodes();
+  }
+}
