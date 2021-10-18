9c8d0420dd5a7e533f097384782e3532ccd12932
dionolympiaCVP <90858230+dionolympiaCVP@users.noreply.github.com>
2021-10-18 14:17:45 -0400

    Feature/1083 mdm endpoints for waf method code (#49)
    * creating files for WAF method code
    
    * added get endpoint for wad method codes
    
    * adding unit tests for waf method codes controller and service
    
    * renaming test cases for waf method codes
    
    * unit tests for waf method code repository
    
    * added correct route for waf method codes

--- src/routes.ts
+++ src/routes.ts
@@ -30,6 +30,7 @@ import { OperatingLevelCodeModule } from './operating-level-code/operating-level
 import { EquationCodeModule } from './equation-code/equation-code.module';
 import { DefaultPurposeCodeModule } from './default-purpose-code/default-purpose-code.module';
 import { FuelCodeModule } from './fuel-code/fuel-code.module';
+import { WafMethodCodeModule } from './waf-method-code/waf-method-code.module';
 import { FuelIndicatorCodeModule } from './fuel-indicator-code/fuel-indicator-code.module';
 import { DemMethodCodeModule } from './dem-method-code/dem-method-code.module';
 
@@ -153,6 +154,10 @@ const routes: Routes = [
   {
     path: '/fuel-codes',
     module: FuelCodeModule,
+  },
+  {
+    path: '/waf-method-codes',
+    module: WafMethodCodeModule,
   },
   {
     path: '/fuel-indicator-codes',
