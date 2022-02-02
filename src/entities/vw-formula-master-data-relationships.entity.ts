037fa31f3d3610517f2a758641582ebe6f2e8fe6
dionolympiaCVP <90858230+dionolympiaCVP@users.noreply.github.com>
2022-02-02 15:33:15 -0500

    renaming parameter code after database was updated (#76)

--- src/entities/vw-formula-master-data-relationships.entity.ts
+++ src/entities/vw-formula-master-data-relationships.entity.ts
@@ -3,7 +3,7 @@ import { ViewColumn, ViewEntity } from 'typeorm';
 @ViewEntity({ name: 'camdecmpsmd.vw_formula_master_data_relationships' })
 export class FormulaRelationships {
   @ViewColumn({
-    name: 'paremeter_code',
+    name: 'parameter_code',
   })
   parameterCode: 'string';
 
