22f1b25d2c8169201ca16e15f45611b152461ce5
Jeffrey Aggrey <jeffreyaggrey@gmailcom>
2022-05-31 19:04:36 -0500

    add ApiTag for test type codes

--- src/test-type-code/test-type-code.controller.ts
+++ src/test-type-code/test-type-code.controller.ts
@@ -3,16 +3,18 @@ import {
   ApiBadRequestResponse,
   ApiOkResponse,
   ApiSecurity,
+  ApiTags,
 } from '@nestjs/swagger';
 import { TestTypeCodeDTO } from '../dto/test-type-code.dto';
 import { TestTypeCodeService } from './test-type-code.service';
 
 @Controller()
+@ApiSecurity('APIKey')
+@ApiTags('Test Type Codes')
 export class TestTypeCodeController {
   constructor(private readonly service: TestTypeCodeService) {}
 
   @Get()
-  @ApiSecurity('APIKey')
   @ApiOkResponse({
     isArray: true,
     type: TestTypeCodeDTO,
