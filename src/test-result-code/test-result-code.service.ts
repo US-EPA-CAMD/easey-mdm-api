510806909c243c70f4bf3d34285f4541f574b3f0
Jeffrey Aggrey <jeffreyaggrey@gmail.com>
2022-07-08 13:29:08 +0000

    fix code smells

--- src/test-result-code/test-result-code.service.ts
+++ src/test-result-code/test-result-code.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Logger } from '@us-epa-camd/easey-common/logger';
 import { TestResultCodeDTO } from '../dto/test-result-code.dto';
 import { TestResultCodeRepository } from './test-result-code.repository';
 
@@ -9,10 +8,9 @@ export class TestResultCodeService {
   constructor(
     @InjectRepository(TestResultCodeRepository)
     private readonly repository: TestResultCodeRepository,
-    private readonly logger: Logger,
   ) {}
 
   async getTestResultCodes(): Promise<TestResultCodeDTO[]> {
-    return await this.repository.find();
+    return this.repository.find();
   }
 }
