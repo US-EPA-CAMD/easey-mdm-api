c91734a90e5d2793369fae291b32f82efa5ff330
Jon Maxwell Diebold <max.diebold@erg.com>
2024-08-07 19:07:48 -0400

    fix(6290): updated version of easey-common

--- src/code-table/code-table.service.ts
+++ src/code-table/code-table.service.ts
@@ -1,9 +1,6 @@
-import {
-  HttpStatus,
-  Injectable,
-} from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { LoggingException } from '@us-epa-camd/easey-common/exceptions';
+import { EaseyException } from '@us-epa-camd/easey-common/exceptions';
 
 import { DataSetDTO } from '../dto/dataset.dto';
 import { DataSetMap } from '../maps/dataset.map';
@@ -26,18 +23,28 @@ export class CodeTableService {
 
   async getCodeTable(codeTable: string): Promise<any[]> {
     const queryColumns = [];
-    const dataSet = await this.repository.getDataSet(codeTable, this.templateCode);
+    const dataSet = await this.repository.getDataSet(
+      codeTable,
+      this.templateCode,
+    );
 
     if (dataSet === null || dataSet === undefined) {
-      throw new LoggingException(`/${codeTable} is not a valid path. Please refer to the list of available code tables using the /list method.`, HttpStatus.BAD_REQUEST);
+      throw new EaseyException(
+        new Error(
+          `/${codeTable} is not a valid path. Please refer to the list of available code tables using the /list method.`,
+        ),
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
     queryColumns.push(
-      dataSet.tables[0].columns.map(col => `${col.name} AS "${col.alias}"`)
-    );    
+      dataSet.tables[0].columns.map(col => `${col.name} AS "${col.alias}"`),
+    );
 
-    const query = dataSet.tables[0].sqlStatement
-      .replace('*', queryColumns.join(','));
+    const query = dataSet.tables[0].sqlStatement.replace(
+      '*',
+      queryColumns.join(','),
+    );
 
     return this.repository.getCodeTable(query);
   }
