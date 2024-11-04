6dfd5252ce0964f416d30fa4c45feb2d62cdd10e
ergjustin <justin.joseph.erg.com>
2024-11-04 14:56:53 -0600

    feat:added ecmps offline middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { RouterModule } from 'nest-router';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,6 +6,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig } from '@us-epa-camd/easey-common/config';
 import { LoggerModule } from '@us-epa-camd/easey-common/logger';
 import { CorsOptionsModule } from '@us-epa-camd/easey-common/cors-options';
+import { MaintenanceMiddleware } from '@us-epa-camd/easey-common/middleware/maintenance.middleware';
+import { HttpModule } from '@nestjs/axios';
 
 import routes from './routes';
 import appConfig from './config/app.config';
@@ -41,6 +43,11 @@ import { RelationshipsModule } from './relationships/relationships.module';
     // MASTER DATA MODULE MUST ALWAYS BE AFTER ALL OTHERS
     // IN ORDER FOR ROUTES TO PROPERLY WORK    
     MasterDataModule,
+    HttpModule
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MaintenanceMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
+}
